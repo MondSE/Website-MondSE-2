d371816cb365cab99f4b7bef11ad16ed8d8b4c1d
Almond Tuazon <azirtuazon@gmail.com>
2025-02-05 22:36:24 +0800

    add project details page

---
+++ src/components/Projects/ProjectsDetails.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ProjectsDetails = () => {
+  return (
+    <div className=" contianer px-4 pt-20 lg:pt-32 mx-auto max-w-screen-lg">
+      <h1 className=" text-3xl lg:text-5xl mb-5 font-bold">title project</h1>
+      <div className=" flex gap-2">
+        <h3 className=" text-pretty text-text-100/60">
+          <font style={{ verticalAlign: "inherit" }}>
+            <font style={{ verticalAlign: "inherit" }}>Technologies:</font>
+          </font>
+        </h3>
+        <div className=" flex flex-wrap gap-2 mb-10">
+          <div className=" py-1 px-2 rounded-md bg-accent-200 flex justify-center gap-2 items-center border text-text-100">
+            <p className=" capitalize text-sm">
+              <font style={{ verticalAlign: "inherit" }}>
+                <font style={{ verticalAlign: "inherit" }}># HTML</font>
+              </font>
+            </p>
+            <p className=" capitalize text-sm">
+              <font style={{ verticalAlign: "inherit" }}>
+                <font style={{ verticalAlign: "inherit" }}># HTML</font>
+              </font>
+            </p>
+            <p className=" capitalize text-sm">
+              <font style={{ verticalAlign: "inherit" }}>
+                <font style={{ verticalAlign: "inherit" }}># HTML</font>
+              </font>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsDetails;
